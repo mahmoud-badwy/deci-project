7f754076b78d03b8db8c4b8b9c3ab8af7dd3dfe7
mahmoud-badwy <mahmoudbadwysliem@gmail.com>
2023-05-19 12:04:47 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#check if player untill play
+isInGame= True
+password = 'mahmoud'
+#  myCommands = {
+#     'go to cenima' : {
+#         '1' : 'play film'
+#         , '2': 'off film'
+#     },
+#     'go to home' : {
+#         'go inSide': ' go outSide'
+#     },
+#     'go to play ground' : {},
+#     'go to cenima' : {},
+# }
+while password == 'mahmoud' :
+    while(isInGame):
+        print('hi')
+    
+
